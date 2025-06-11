4e0f29794eac9b1e0ea70c3322f93fcd7c9af93a
John Dykstra <john.dykstra1@gmail.com>
2025-06-11 16:57:33 -0500

    Finish this restructuring.

--- quodlibet/browsers/dspconfig.py
+++ quodlibet/browsers/dspconfig.py
@@ -11,7 +11,11 @@ from ..player.dsp import DspController, dsp_controller
 import quodlibet.qltk.touch as tt
 
 
-class ConfigSelector(Gtk.VBox):
+class ConfigChooser(Gtk.VBox):
+
+    SELECTED_COLOR = tt.TouchTile.GREEN
+    UNSELECTED_COLOR = tt.TouchTile.BLUE
+
     def __init__(self, browser):
         super().__init__(spacing=10)
         self.browser = browser
@@ -31,20 +35,19 @@ class ConfigSelector(Gtk.VBox):
         self.rect_buttons = []
         self.button_to_config = {}
         self.config_buttons_box = Gtk.VBox()
-        self.create_config_chooser()
-        for button in self.rect_buttons:
-            self.config_buttons_box.pack_start(button, False, False, 0)
+        self._create_buttons()
         self.pack_start(self.config_buttons_box, False, False, 0)
 
-    def create_config_chooser(self):
+    def _create_buttons(self):
         """
         Create rectangle buttons for each configuration file, colored red if unselected and green if selected.
         """
         try:
             dsp_controller.connect()
             self.config_dir, config_files = dsp_controller.get_configs()
+            self.config_files = sorted(config_files)
             current_path = dsp_controller.config.file_path()
-            current_file = os.path.basename(current_path) if current_path else None
+            self._current_config = os.path.basename(current_path) if current_path else None
         except Exception as e:
             error_label = Gtk.Label(label=f"Error: {e}")
             self.pack_start(error_label, False, False, 0)
@@ -52,32 +55,16 @@ class ConfigSelector(Gtk.VBox):
         finally:
             dsp_controller.disconnect()
             
-        self.rect_buttons = []
-        self.button_to_config = {}
-        config_files = list(config_files)  
-        for config_file in config_files:
-            button = tt.TouchTile(label=config_file)
-            button.set_relief(Gtk.ReliefStyle.NONE)
-            button.set_size_request(-1, 25)
-            button.connect("clicked", self.on_rect_button_clicked, config_file)
-            self.rect_buttons.append(button)
-            self.button_to_config[button] = config_file
+        for f in self.config_files:
+            button = tt.TouchTile(label=f, color=self.UNSELECTED_COLOR)
+            if f == self._current_config:
+                button.set_color(self.SELECTED_COLOR)
+                self._selected_button = button
+            button.connect("clicked", self._on_rect_button_clicked, f)
+            self.config_buttons_box.pack_start(button, False, False, 0)
 
-        # Select the button matching the current config file, if any
-        #  ??  Change the color, but don't command the DSP.
-        #  ??  There's got to be a a way to do with with button_to_config.
-        selected_button = None
-        for button, config_file in self.button_to_config.items():
-            if config_file == current_file:
-                selected_button = button
-                break
 
-        if selected_button:
-            self.select_rect_button(selected_button, current_file)
-        elif self.rect_buttons:
-            self.select_rect_button(self.rect_buttons[0], config_files[0])
-
-    def select_rect_button(self, selected_button, new_config):
+    def _change_selected_config(self, selected_button, new_config):
         try:
             dsp_controller.connect()
             dsp_controller.config.set_file_path(os.path.join(self.config_dir, new_config))
@@ -89,13 +76,14 @@ class ConfigSelector(Gtk.VBox):
         finally:
             dsp_controller.disconnect()
 
-        for button in self.rect_buttons:
-            newColor = tt.TouchTile.GREEN if button == selected_button else tt.TouchTile.BLUE
-            button.set_color(newColor)
-        self.selected_config = new_config
 
-    def on_rect_button_clicked(self, button, new_config):
-        self.select_rect_button(button, new_config)
+    def _on_rect_button_clicked(self, button, new_config):
+        self._change_selected_config(button, new_config)
+        if self._selected_button:
+            self._selected_button.set_color(self.UNSELECTED_COLOR)
+        button.set_color(self.SELECTED_COLOR)    
+        self._current_config = new_config
+        self._selected_button = button
 
 
 class DspControlWindow(qltk.UniqueWindow):
@@ -107,11 +95,12 @@ class DspControlWindow(qltk.UniqueWindow):
         self.set_default_size(350, 200)
         self.set_border_width(12)
         self.set_title("Camilla DSP")
-        vbox = ConfigSelector(browser)
-        self.add(vbox)
+        configChooser = ConfigChooser(browser)
+        self.add(configChooser)
         self.get_child().show_all()
 
-class DspWindowButton(Gtk.HBox):
+
+class DspWindowOpener(Gtk.HBox):
     """
     Click this button to open the DSP configuration window.
     """
